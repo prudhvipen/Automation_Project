26ca9d4dd7c9ebfc04f8c80e3c7aface33120aa2
root <root@ip-172-31-28-51.ec2.internal>
2022-07-06 11:16:46 +0000

    task3

--- automation.sh
+++ automation.sh
@@ -28,8 +28,71 @@ fi
 #making tar file of /var/log/apache2 log files
 timestamp=$(date '+%d%m%Y-%H%M%S')
 
-cd /home/ubuntu/tmp/
+cd
+cd tmp
 tar -zcvf ${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/access.log /var/log/apache2/error.log
 
 #Copying the tar file into AWS s3 bucket
 aws s3 cp ${myname}-httpd-logs-${timestamp}.tar $s3storage
+
+#To check if the /var/www/html/inventory.html exists and creating if needed
+if
+[ -e /var/www/html/inventory.html ]
+then
+echo "inventory.html is found"
+else
+touch /var/www/html/inventory.html
+fi
+
+
+#To check if the header is present or to append if not present
+file=/var/www/html/inventory.html
+heading="LogType                TimeCreated             Type                    Size"
+
+if grep -wo "LogType" $file 
+then
+echo "Header found"
+else
+echo    ${heading} >> $file
+fi
+
+
+#Appending the Tar archive entry
+tarfile=${myname}-httpd-logs-${timestamp}.tar
+
+LogType=httpd
+TimeCreated=$timestamp
+Type=tar
+Size=$(ls -lh $tarfile | awk '{print  $5}')
+
+newentry=${LogType}"		"${TimeCreated}"		"${Type}"		"${Size}K
+
+echo $newentry>>$file
+
+
+#to check if the cron.d directory exists and to create if it is not.
+cd /etc/
+dir="/etc/cron.d/"
+
+if [ -d "$dir" ] 
+then
+echo "cron.d folder exists"
+else
+mkdir cron.d
+fi
+
+#Creating the cron automation file
+
+if
+[ -e automation ]
+then
+rm automation
+else
+exit
+fi
+
+#creating the cron schedule
+touch  /etc/cron.d/automation
+echo "* 12 * * * root  /root/Automation_Project/automation.sh" >> automation
+chmod +x automation
+
